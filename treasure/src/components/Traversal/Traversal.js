62ec81e1781541bb4f3187b97637cb399b9633ed
Kevin <brackkevin@gmail.com>
2019-02-04 12:49:09 -0600

    Adds init method and axios request, added to CDM

--- treasure/src/components/Traversal/Traversal.js
+++ treasure/src/components/Traversal/Traversal.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Traversal extends Component {
   constructor(props) {
@@ -9,11 +10,43 @@ class Traversal extends Component {
         0: { n: "?", s: "?", w: "?", e: "?" }
       },
       current_path: [],
-      num_explored: 1
+      num_explored: 1,
+      last_response: {},
+      config: {
+        headers: {
+          Authorization: "Token " + process.env.REACT_APP_API_KEY
+        }
+      }
     };
   }
+
+  componentDidMount() {
+    this.init();
+  }
+
+  init = () => {
+    const config = this.state.config;
+    let url = "https://lambda-treasure-hunt.herokuapp.com/api/adv/init/";
+    axios
+      .get(url, config)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  pick_unexplored = () => {};
+
+  move_player = () => {};
+
+  log_traversal = () => {};
+
+  reverse_direction = () => {};
+
   render() {
-    return <div />;
+    return <div>TRAVERSAL</div>;
   }
 }
 
