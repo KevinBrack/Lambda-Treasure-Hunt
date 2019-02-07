981d7b3e88ab8bc7bb4160a7683f051a508b4b53
Kevin <brackkevin@gmail.com>
2019-02-07 11:27:15 -0600

    Preparing to hoist necessary shared state to App

--- treasure/src/App.js
+++ treasure/src/App.js
@@ -4,11 +4,34 @@ import "./App.css";
 import Traversal from "./components/Traversal/Traversal";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      graph: {},
+      current_room: null,
+      show_ui: true
+    };
+  }
+
+  update_graph_handler = graph => {
+    this.setState({ graph });
+  };
+
+  update_current_room_handler = current_room => {
+    this.setState({ current_room });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Traversal />
+          <Traversal
+            update_graph_handler={this.update_state_handler}
+            update_current_room_handler={this.update_current_room_handler}
+            graph={this.state.graph}
+            current_room={this.state.current_room}
+            show_ui={this.state.show_ui}
+          />
         </header>
       </div>
     );
