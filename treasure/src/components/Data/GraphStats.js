34835f7ace1e15d7ecd81ce915cc86bcff917d60
Kevin <brackkevin@gmail.com>
2019-02-07 14:46:02 -0600

    Changes graph stats to accept live input and return a single object

--- treasure/src/components/Data/GraphStats.js
+++ treasure/src/components/Data/GraphStats.js
@@ -1,6 +1,18 @@
-import graph from "./graph";
+const graph_stats = graph => {
+  let result = {};
+  let graph_arr_result = graph_arr(graph);
+  result.min_max_cords = min_max_cords(graph_arr_result);
+  result.graph_arr = graph_arr_result;
+  result.arrays = calculate_grid_size(
+    result.min_max_cords.max_x,
+    result.min_max_cords.min_x,
+    result.min_max_cords.max_y,
+    result.min_max_cords.min_y
+  );
 
-const graph_stats = {};
+  console.log(result); // <-- Debugging
+  return result;
+};
 
 const graph_arr = function(graph) {
   const all_cords = [];
@@ -12,34 +24,40 @@ const graph_arr = function(graph) {
   return all_cords;
 };
 
-const GraphArr = graph_arr(graph);
-
 const min_max_cords = function(GraphArr) {
-  const result = [
-    Number(GraphArr[0][0]),
-    Number(GraphArr[0][0]),
-    Number(GraphArr[0][1]),
-    Number(GraphArr[0][1])
-  ];
+  const result = {
+    max_x: Number(GraphArr[0][0]),
+    min_x: Number(GraphArr[0][0]),
+    max_y: Number(GraphArr[0][1]),
+    min_y: Number(GraphArr[0][1])
+  };
   for (let i = 1; i < GraphArr.length; i++) {
-    if (GraphArr[i][0] > result[0]) {
-      result[0] = GraphArr[i][0];
+    if (GraphArr[i][0] > result.max_x) {
+      result.max_x = GraphArr[i][0];
     }
-    if (GraphArr[i][0] < result[1]) {
-      result[1] = GraphArr[i][0];
+    if (GraphArr[i][0] < result.min_x) {
+      result.min_x = GraphArr[i][0];
     }
-    if (GraphArr[i][1] > result[2]) {
-      result[2] = GraphArr[i][1];
+    if (GraphArr[i][1] > result.max_y) {
+      result.max_y = GraphArr[i][1];
     }
-    if (GraphArr[i][1] < result[3]) {
-      result[3] = GraphArr[i][1];
+    if (GraphArr[i][1] < result.min_y) {
+      result.min_y = GraphArr[i][1];
     }
   }
   return result;
 };
 
-graph_stats.all_cords = graph_arr(graph);
-graph_stats.min_max_cords = min_max_cords(GraphArr);
-graph_stats.graph = graph;
+let calculate_grid_size = (max_x, min_x, max_y, min_y) => {
+  let x_arr = [];
+  let y_arr = [];
+  for (let i = min_x; i <= max_x; i++) {
+    x_arr.push(i);
+  }
+  for (let i = min_y; i <= max_y; i++) {
+    y_arr.push(i);
+  }
+  return { x: x_arr, y: y_arr };
+};
 
 export default graph_stats;
